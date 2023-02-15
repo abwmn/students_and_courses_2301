538c840d79ad3f2ec3f56da2ef12f59ea73a67f3
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-15 11:25:53 -0500

    feat: add log_score and grade methods

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,17 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
 
   def initialize(details)
     @name = details[:name]
     @age = details[:age]
     @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum.to_f / @scores.length
   end
 end
