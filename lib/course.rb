9db2a6daa41cc83aa7f29741598c85d7554d9d0e
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-15 11:32:16 -0500

    feat: initialize course with attributes

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,14 @@
+class Course
+attr_reader :name, :capacity
 
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    
+  end
+
+end
