2e9f2754d9502cf95cef6819ad62b6ca032fd948
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-15 11:44:29 -0500

    test: test enroll and full? methods

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe Course do
     expect(@course.name).to eq("Calculus")
     expect(@course.capacity).to eq(2)
   end
+
+  it 'can enroll students' do
+    expect(@course.students).to eq([])
+
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})   
+    @course.enroll(student1)
+    @course.enroll(student2)
+
+    expect(@course.students).to eq([student1, student2])
+
+    student3 = Student.new({name: "Lonely", age: 24})
+    @course.enroll(student3)
+
+    expect(@course.students).to eq([student1, student2])
+  end
+
+  it 'can check if full' do
+    expect(@course.full?).to eq(false)
+
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})   
+    @course.enroll(student1)
+    @course.enroll(student2)
+
+    expect(@course.full?).to eq(true)
+  end
+
 end
