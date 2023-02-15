28abd0cb70d27d70e7af33c28e5b338aacdc1074
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-15 12:45:07 -0500

    feat: add gradebook with functionality

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -6,4 +6,27 @@ class Gradebook
     @courses = []
   end
 
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    all_students = {}
+    @courses.each do |course|
+      all_students[course] = course.students
+    end
+    all_students
+  end
+
+  def students_below(threshold)
+    students_below = []
+    list_all_students.values.each do |course|
+      course.each do |student|
+        if student.grade < threshold
+          students_below << student
+        end
+      end
+    end
+    students_below
+  end
 end
